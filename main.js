c0f93eb15d33d9d8522b3464d105e4598b212520
DebarsitaMaiti2021 <83021421+DebarsitaMaiti2021@users.noreply.github.com>
2021-06-05 19:58:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("My First Console Project");
+first_name="Debarsita";
+last_name="Maiti";
+full_name=first_name.concat(last_name);
+console.log(full_name);
+a=8;
+b=4;
+add=a+b;
+console.log(add);
+
+sub=a-b;
+console.log(sub);
+
+mul=a*b;
+console.log(mul);
+
+division=a/b;
+console.log(division);
